caa4f6ac019d2ca2e31d04caed32c11784739d00
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-07 20:37:10 -0300

    [feature] user defaults

--- userDefaults/ViewController.swift
+++ userDefaults/ViewController.swift
@@ -21,19 +21,29 @@ class ViewController: UIViewController {
     @IBAction func getAction(_ sender: Any) {
         if let value = UserDefaults.standard.string(forKey: kMyKey) {
             print("Hay un dato guardado", value)
+            showAlert(message: value)
         } else {
             print("No hay datos guardados")
+            showAlert(message: "No hay datos guardados")
         }
     }
     
     @IBAction func putAction(_ sender: Any) {
         UserDefaults.standard.set("BOKEEE", forKey: kMyKey)
         UserDefaults.standard.synchronize() //forzamos que lo guarde, no seria necesario
+        showAlert(message: "Hemos guardado el dato")
     }
     
     @IBAction func deleteAction(_ sender: Any) {
         UserDefaults.standard.removeObject(forKey: kMyKey)
-        UserDefaults.standard.synchronize() 
+        UserDefaults.standard.synchronize()
+        showAlert(message: "Hemos eliminado el dato")
+    }
+    
+    private func showAlert(message:String){
+        let alert = UIAlertController(title: "My user defaults", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
+            present(alert, animated: true)
     }
 }
 
